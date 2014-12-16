81004e9bdc1b0cc0cce770ddd79039f1506716e1
Abraham <abraham_starosta@abrahams-mbp-2.gateway.2wire.net>
2014-12-15 23:50:22 -0500

    Sever

--- server.py
+++ server.py
@@ -1,9 +1,16 @@
 from flask import Flask
+import flask.views
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+class View(flask.views.MethodView):
+	def get(self):
+		return "Hello World!"
+
+app.add_url_rule('/', view_func=View.as_view('main'))
+
+app.debug = True
+
 
 if __name__ == "__main__":
     app.run()
