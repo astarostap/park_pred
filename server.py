953f09774e73ec871a7c2af3d7e95a064292b010
Abraham <abraham_starosta@abrahams-mbp-2.gateway.2wire.net>
2014-12-22 23:23:58 -0500

    +

--- server.py
+++ server.py
@@ -1,12 +1,11 @@
 from flask import Flask
-import flask.views
 
 app = Flask(__name__)
 
 @app.route("/")
-def server():
+def hello():
 	return "Hello Abraham and Baris.!"
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
